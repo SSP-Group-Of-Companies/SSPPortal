fd13de870fa4de4b0c4affb8179b9ce131ad54f5
faruqssp <faruq.atanda@sspgroup.com>
2026-07-10 10:59:42 -0400

    chore: update comment in root layout to trigger redeploy

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-// Root layout for SSP Portal App, 
+// Root layout for SSP Portal App,trigger redeploy
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
